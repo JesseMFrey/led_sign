a42691210952dd6ddd96028072ba1b86e3060796
Jesse Frey <jesse.m.frey@gmail.com>
2023-10-09 14:07:44 -0600

    Initial commit

---
+++ led_sign.ino
@@ -0,0 +1,41 @@
+//#define FASTLED_FORCE_SOFTWARE_PINS
+
+#include "FastLED.h"
+
+#define LED_PIN A0
+
+#define NUM_LEDS (14)
+
+CRGB leds[NUM_LEDS];
+
+enum pattern {PAT_FLOW=0, PAT_FADE};
+
+void setup() {
+  //turn all LEDs off
+  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+  
+  FastLED.addLeds<WS2812, LED_PIN,GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness( 255 ); // out of 255
+}
+
+int pos = 0;
+enum pattern pat=PAT_FLOW;
+
+void loop() {
+
+  switch(pat)
+  {
+    case PAT_FLOW:
+      pos += 1;
+      fill_rainbow(leds, NUM_LEDS, pos, max(1, 255 / NUM_LEDS) );
+      delay(20);
+      break;
+    case PAT_FADE:
+      pos += 1;
+      fill_solid(leds, NUM_LEDS, CHSV(pos, 255, 255));
+      delay(50);
+      break;
+  }
+  FastLED.show();
+
+}
